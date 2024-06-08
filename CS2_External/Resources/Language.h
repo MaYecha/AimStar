b45b6503be0323e5d9ac46d6ca1a4867c5df4e0b
MaYecha <145244567+MaYecha@users.noreply.github.com>
2024-06-08 23:37:45 +0800

    Update Language.h

--- CS2_External/Resources/Language.h
+++ CS2_External/Resources/Language.h
@@ -1,4 +1,4 @@
-#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+﻿#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
 #pragma once
 #include "..\Font\IconsFontAwesome5.h"
 #include <iostream>
@@ -235,165 +235,166 @@ namespace Lang
 		Global.FeatureSettings = u8"Settings";
 
 		// ESP
-		ESPtext.Toggle = u8"总开关";
+		ESPtext.Toggle = u8"Toggle";
 		ESPtext.FeatureName = u8" ESP";
-		ESPtext.Box = u8"方框";
-		ESPtext.BoxRounding = u8"圆角化:";
-		ESPtext.FilledBox = u8"填充方框";
-		ESPtext.FilledAlpha = u8"填充透明度";
-		ESPtext.Skeleton = u8"骨骼";
-		ESPtext.HeadBox = u8"头部方框";
-		ESPtext.EyeRay = u8"视线朝向";
-		ESPtext.HealthBar = u8"生命栏";
-		ESPtext.Weapon = u8"武器";
-		ESPtext.Distance = u8"距离";
-		ESPtext.PlayerName = u8"玩家名";
-		ESPtext.SnapLine = u8"射线";
-		ESPtext.LinePosList = u8"射线位置:";
-		ESPtext.VisCheck = u8"可视检查";
-		ESPtext.Preview = u8"效果预览";
-		ESPtext.CollapseHead = u8"性感设置";
-		ESPtext.Penis = u8"显示牛子";
-		ESPtext.PenisLength = u8"牛子长度";
-		ESPtext.PenisSize = u8"牛子粗细";
-		ESPtext.MultiColor = u8"渐变色";
-		ESPtext.MultiColTip = u8"仅在圆角度为0时显示.";
-		ESPtext.Outline = u8"轮廓线";
-		ESPtext.BoxType = u8"方框样式:";
-		ESPtext.HealthNum = u8"生命数值";
-		ESPtext.Ammo = u8"弹夹栏";
-		ESPtext.ScopedESP = u8"开镜状态";
-		ESPtext.ShowArmorBar = u8"盔甲栏";
-		ESPtext.ArmorNum = u8"盔甲数值";
-		ESPtext.RenderDistance = u8"最远显示距离: ";
+		ESPtext.Box = u8"Frame";
+		ESPtext.BoxRounding = u8"Box Rounding: ";
+		ESPtext.FilledBox = u8"Filled Box";
+		ESPtext.FilledAlpha = u8"Filled Box Alpha";
+		ESPtext.Skeleton = u8"Skeleton";
+		ESPtext.HeadBox = u8"Head Box";
+		ESPtext.EyeRay = u8"Eye Ray";
+		ESPtext.HealthBar = u8"Health Bar";
+		ESPtext.Weapon = u8"Weapon";
+		ESPtext.Distance = u8"Distance";
+		ESPtext.PlayerName = u8"Name";
+		ESPtext.SnapLine = u8"Snap Line";
+		ESPtext.LinePosList = u8"Line Position: ";
+		ESPtext.VisCheck = u8"Visible Check";
+		ESPtext.Preview = u8"Preview Window";
+		ESPtext.CollapseHead = u8"SexyESP";
+		ESPtext.Penis = u8"Show Penis";
+		ESPtext.PenisLength = u8"Length";
+		ESPtext.PenisSize = u8"Size";
+		ESPtext.MultiColor = u8"Multi-Color";
+		ESPtext.MultiColTip = u8"Only work when the box has non-rounded corners.";
+		ESPtext.Outline = u8"Outline";
+		ESPtext.BoxType = u8"Box Type:";
+		ESPtext.HealthNum = u8"Health Number";
+		ESPtext.Ammo = u8"Ammo";
+		ESPtext.ScopedESP = u8"Show Scoped";
+		ESPtext.ShowArmorBar = u8"Armor Bar";
+		ESPtext.ArmorNum = u8"Armor Number";
+		ESPtext.RenderDistance = u8"Maximum Render Distance: ";
 
-		ESPtext.BoxType_Normal = u8"正常";
-		ESPtext.BoxType_Edge = u8"动态";
-		ESPtext.BoxType_Corner = u8"四角";
-		ESPtext.LinePos_1 = u8"顶部";
-		ESPtext.LinePos_2 = u8"中心";
-		ESPtext.LinePos_3 = u8"底部";
+		ESPtext.BoxType_Normal = u8"Normal";
+		ESPtext.BoxType_Edge = u8"Dynamic";
+		ESPtext.BoxType_Corner = u8"Corner";
+		ESPtext.LinePos_1 = u8"Top";
+		ESPtext.LinePos_2 = u8"Center";
+		ESPtext.LinePos_3 = u8"Bottom";
 
 		// Aimbot
-		AimbotText.Enable = u8"启用自瞄";
-		AimbotText.FeatureName = u8" 瞄准辅助";
-		AimbotText.HotKeyList = u8"热键        ";
-		AimbotText.Toggle = u8"开关模式";
-		AimbotText.DrawFov = u8"显示范围";
-		AimbotText.VisCheck = u8"仅可见目标";
-		AimbotText.JumpCheck = u8"仅在地面上";
-		AimbotText.FovSlider = u8"自瞄范围: ";
-		AimbotText.FovMinSlider = u8"最小自瞄范围: ";
-		AimbotText.SmoothSlider = u8"平滑度: ";
-		AimbotText.BoneList = u8"瞄准部位  ";
-		AimbotText.Tip = u8"此功能在菜单打开时不会工作";
-		AimbotText.ScopeOnly = u8"仅开镜";
-		AimbotText.AimLock = u8"自动锁定";
-		AimbotText.AutoShot = u8"自动开火";
-		AimbotText.BulletSlider = u8"从第几枪开始: ";
-		AimbotText.IgnoreFlash = u8"无视闪光弹自瞄";
-		AimbotText.Ragebot = u8"暴力模式";
+		AimbotText.Enable = u8"Enable Aimbot";
+		AimbotText.FeatureName = u8" Aimbot";
+		AimbotText.HotKeyList = u8"Hotkey   ";
+		AimbotText.Toggle = u8"Toggle Mode";
+		AimbotText.DrawFov = u8"Draw Fov";
+		AimbotText.VisCheck = u8"Visible Only";
+		AimbotText.JumpCheck = u8"On Ground Only";
+		AimbotText.FovSlider = u8"FOV: ";
+		AimbotText.FovMinSlider = u8"MinFOV: ";
+		AimbotText.SmoothSlider = u8"Smooth: ";
+		AimbotText.BoneList = u8"Hitbox";
+		AimbotText.Tip = u8"Aimbot will not work while the menu is opened";
+		AimbotText.ScopeOnly = u8"Scope Only";
+		AimbotText.AimLock = u8"Aim Lock";
+		AimbotText.AutoShot = u8"Auto Shot";
+		AimbotText.BulletSlider = u8"Start Bullet: ";
+		AimbotText.IgnoreFlash = u8"Ignore Flash ";
+		AimbotText.Ragebot = u8"Ragebot";
 
 		// RCS
-		RCStext.Toggle = u8"后座修正";
-		RCStext.Yaw = u8"水平偏移量: ";
-		RCStext.Pitch = u8"竖直偏移量: ";
+		RCStext.Toggle = u8"Recoil Control";
+		RCStext.Yaw = u8"Yaw: ";
+		RCStext.Pitch = u8"Pitch: ";
 
 		// Radar
-		RadarText.Toggle = u8"显示雷达";
-		RadarText.FeatureName = u8" 外部雷达";
-		RadarText.StyleList = u8"雷达样式";
-		RadarText.CustomCheck = u8"自定义设置";
-		RadarText.CrossLine = u8"十字线";
-		RadarText.SizeSlider = u8"标记大小:";
-		RadarText.ProportionSlider = u8"缩放比例:";
-		RadarText.RangeSlider = u8"范围:";
-		RadarText.AlphaSlider = u8"窗口透明度:";
+		RadarText.Toggle = u8"Show Radar";
+		RadarText.FeatureName = u8" Radar";
+		RadarText.StyleList = u8"Style";
+		RadarText.CustomCheck = u8"Custom";
+		RadarText.CrossLine = u8"Cross Line";
+		RadarText.SizeSlider = u8"Point Size:";
+		RadarText.ProportionSlider = u8"Proportion:";
+		RadarText.RangeSlider = u8"Range:";
+		RadarText.AlphaSlider = u8"Window Background Alpha:";
 
 		// Triggerbot
-		TriggerText.Enable = u8"启用扳机";
-		TriggerText.FeatureName = u8" 自动扳机";
-		TriggerText.HotKeyList = u8"热键    ";
-		TriggerText.Toggle = u8"保持激活";
-		TriggerText.ScopeOnly = u8"仅开镜";
-		TriggerText.DelaySlider = u8"延迟射击:";
-		TriggerText.FakeShotSlider = u8"射击持续时间:";
+		TriggerText.Enable = u8"Enable Triggerbot";
+		TriggerText.FeatureName = u8" Triggerbot";
+		TriggerText.HotKeyList = u8"Hotkey   ";
+		TriggerText.Toggle = u8"Always Active";
+		TriggerText.ScopeOnly = u8"Scope Only";
+		TriggerText.DelaySlider = u8"Shot Delay:";
+		TriggerText.FakeShotSlider = u8"Shot Duration:";
 
 		// Crosshairs
-		CrosshairsText.Toggle = u8"显示准星";
-		CrosshairsText.FeatureName = u8" 外部准星";
-		CrosshairsText.PresetList = u8"预设";
-		CrosshairsText.ColorEditor = u8"准星颜色";
-		CrosshairsText.Dot = u8"中心点";
-		CrosshairsText.DotSizeSlider = u8"中心点大小:";
-		CrosshairsText.Outline = u8"轮廓线";
-		CrosshairsText.Crossline = u8"十字线";
-		CrosshairsText.hLengthSlider = u8"水平长度:";
-		CrosshairsText.vLengthSilder = u8"垂直长度:";
-		CrosshairsText.GapSlider = u8"间隙:";
-		CrosshairsText.ThicknessSlider = u8"粗细:";
-		CrosshairsText.tStyle = u8"T形准星";
-		CrosshairsText.Circle = u8"圆环";
-		CrosshairsText.RadiusSlider = u8"圆环半径:";
-		CrosshairsText.TargetCheck = u8"瞄准变色";
-		CrosshairsText.TeamCheck = u8"队伍检查";
+		CrosshairsText.Toggle = u8"Show Crosshairs";
+		CrosshairsText.FeatureName = u8" Crosshairs";
+		CrosshairsText.PresetList = u8"Presets";
+		CrosshairsText.ColorEditor = u8"Crosshairs Color";
+		CrosshairsText.Dot = u8"Center Dot";
+		CrosshairsText.DotSizeSlider = u8"Dot Size:";
+		CrosshairsText.Outline = u8"Outline";
+		CrosshairsText.Crossline = u8"Crossline";
+		CrosshairsText.hLengthSlider = u8"Horizontal Length:";
+		CrosshairsText.vLengthSilder = u8"Vertical Length:";
+		CrosshairsText.GapSlider = u8"Gap:";
+		CrosshairsText.ThicknessSlider = u8"Thickness:";
+		CrosshairsText.tStyle = u8"T Style";
+		CrosshairsText.Circle = u8"Circle";
+		CrosshairsText.RadiusSlider = u8"Circle Radius:";
+		CrosshairsText.TargetCheck = u8"Targeting Crosshairs";
+		CrosshairsText.TeamCheck = u8"TeamCheck";
 
 		// Misc 
-		MiscText.FeatureName = u8" 杂项功能";
-		MiscText.ThemeList = u8"主题配色       ";
-		MiscText.StyleList = u8"窗口样式";
-		MiscText.HeadshotLine = u8"爆头线";
-		MiscText.SpecCheck = u8"旁观者保持运行";
-		MiscText.NoFlash = u8"无视闪光弹";
-		MiscText.FastStop = u8"自动急停";
-		MiscText.HitSound = u8"命中音效 ";
-		MiscText.bmbTimer = u8"C4计时器";
-		MiscText.SpecList = u8"旁观者列表";
-		MiscText.Bhop = u8"自动连跳";
-		MiscText.Watermark = u8"水印浮窗";
-		MiscText.CheatList = u8"功能列表";
-		MiscText.TeamCheck = u8"阵营判断";
-		MiscText.AntiRecord = u8"过OBS";
-		MiscText.MoneyService = u8"经济系统";
-		MiscText.ShowCashSpent = u8"显示消费";
-		MiscText.EnemySensor = u8"发光";
-		MiscText.RadarHack = u8"小地图雷达";
-		MiscText.VisCheckDisable = u8"可视检查已关闭";
-		MiscText.NoSmoke = u8"移除烟雾";
-		MiscText.SmokeColor = u8"烟雾颜色";
-		MiscText.fovchanger = u8"FOV修改器: ";
-		MiscText.ForceScope = u8"总是开镜";
-		MiscText.FlashImmunity = u8"闪光免疫成都: ";
-		MiscText.NightMode = u8"深色模式";
-		MiscText.Alpha = u8"程度: ";
-		MiscText.JumpThrow = u8"跳投";
+		MiscText.FeatureName = u8" Misc";
+		MiscText.ThemeList = u8"Skin       ";
+		MiscText.StyleList = u8"Style";
+		MiscText.HeadshotLine = u8"Headshot Line";
+		MiscText.SpecCheck = u8"Cheat in Spec";
+		MiscText.NoFlash = u8"No Flash";
+		MiscText.FastStop = u8"Fast Stop";
+		MiscText.HitSound = u8"Hit Sound ";
+		MiscText.bmbTimer = u8"Bomb Timer";
+		MiscText.SpecList = u8"Spec List";
+		MiscText.Bhop = u8"Auto Jump";
+		MiscText.Watermark = u8"Watermark";
+		MiscText.CheatList = u8"Cheat List";
+		MiscText.TeamCheck = u8"Team Check";
+		MiscText.AntiRecord = u8"Anti Record";
+		MiscText.MoneyService = u8"Money Services";
+		MiscText.ShowCashSpent = u8"Show Cash Spent";
+		MiscText.EnemySensor = u8"Glow";
+		MiscText.RadarHack = u8"Radar Hack";
+		MiscText.FastStop = u8"Fast Stop";
+		MiscText.VisCheckDisable = u8"Visible Check DISABLED";
+		MiscText.NoSmoke = u8"No Smoke";
+		MiscText.SmokeColor = u8"Smoke Color";
+		MiscText.fovchanger = u8"Fov Changer: ";
+		MiscText.ForceScope = u8"Force Scope";
+		MiscText.FlashImmunity = u8"Flash Immunity: ";
+		MiscText.NightMode = u8"Night Mode";
+		MiscText.Alpha = u8"Alpha: ";
+		MiscText.JumpThrow = u8"Jump Throw";
 
-		MiscText.FakeDuck = u8"假蹲";
+		MiscText.FakeDuck = u8"Fake Duck";
 
-		MiscText.LanguageList = u8"语言";
+		MiscText.LanguageList = u8"Language";
 
 		// Config Menu
-		ConfigText.FeatureName = u8" 参数配置";
-		ConfigText.MyConfigs = u8"我的配置";
-		ConfigText.Load = u8"加载选中";
-		ConfigText.Save = u8"保存选中";
-		ConfigText.Delete = u8"删除选中";
-		ConfigText.Reset = u8"重置配置";
-		ConfigText.Create = u8"创建配置";
-		ConfigText.OpenFolder = u8"打开文件夹";
-		ConfigText.SeparateLine = u8"新建配置";
-		ConfigText.AuthorName = u8"作者";
-		ConfigText.ConfigName = u8"配置名";
+		ConfigText.FeatureName = u8"Config Loader";
+		ConfigText.MyConfigs = u8"Config List";
+		ConfigText.Load = u8"Load Selected";
+		ConfigText.Save = u8"Save Selected";
+		ConfigText.Delete = u8"Delete Selected";
+		ConfigText.Reset = u8"Reset Config";
+		ConfigText.Create = u8"Create";
+		ConfigText.OpenFolder = u8"Open Folder";
+		ConfigText.SeparateLine = u8"Create Config";
+		ConfigText.AuthorName = u8"Author Name";
+		ConfigText.ConfigName = u8"Config Name";
 
-		ConfigText.SafeMode = u8"安全模式";
-		ConfigText.fpsCap = u8"绘制刷新率: ";
-		ConfigText.SafeModeHoveredTip = u8"关闭所有修改内存的功能";
+		ConfigText.SafeMode = u8"Safe Mode";
+		ConfigText.fpsCap = u8"Frame Limit: ";
+		ConfigText.SafeModeHoveredTip = u8"Disable all unsafe functions that modify game's memory";
 
 		// Readme Menu
-		ReadMeText.FeatureName = u8"README";
-		ReadMeText.LastUpdate = u8"最后更新时间: ";
-		ReadMeText.SourceButton = u8" ";
-		ReadMeText.DiscordButton = u8" ";
-		ReadMeText.OffsetsTitle = u8"基址:";
+		ReadMeText.FeatureName = u8" README";
+		ReadMeText.LastUpdate = u8"Last update: ";
+		ReadMeText.SourceButton = u8"Source Code";
+		ReadMeText.DiscordButton = u8"Join Discord";
+		ReadMeText.OffsetsTitle = u8"Offsets:";
 	}
 }
